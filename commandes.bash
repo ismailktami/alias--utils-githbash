aaa3c3db538a809260465ec5cc79c7c9ca5246c1
Ismail Ktami <ismail.ktami@cdbdx.biz>
2022-04-07 22:22:44 +0200

    add some commande for java docker

--- commandes.bash
+++ commandes.bash
@@ -4,3 +4,16 @@ set lj=java -jar
 
 
 netstat -ano | findstr :8080
+
+# get java process and run gabrafe collector
+ps -fC java
+ps -A |grep java
+java -XX:+PrintFlagsFinal -version | grep HeapSize
+jcmd PID GC.run  //force gabrage collector
+kill -9 <PID>
+
+
+java -jar app.jar -Xmx1g
+
+
+ docker run -d --name=prometheus -p 9090:9090 -v prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --config.file=/etc/prometheus/prometheus.yml
